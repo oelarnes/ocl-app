3dd3d5b9a749f0d791695989826f2660bb2c0394
Joel Barnes <oelarnes@gmail.com>
2020-05-23 15:28:03 -0400

    whitelist booster rules

--- scripts/download_booster_rules.js
+++ scripts/download_booster_rules.js
@@ -5,6 +5,15 @@ const {getBoosterRulesVersion, getCardByUuid, getSet, saveBoosterRules} = requir
 const URL = "https://raw.githubusercontent.com/taw/magic-sealed-data/master/sealed_basic_data.json";
 const REPO_URL = "https://api.github.com/repos/taw/magic-sealed-data/git/refs/heads/master";
 
+
+const whitelistSets = ["9ED", "DIS", "WWK", "MRD", "ME2", "ME4", "MMA", "THS", "RAV", "ROE", "UDS", "5DN", "AER", "EXO", "MOR", "IKO", "USG",
+  "EMA", "FUT", "ME1", "PZ2", "DST", "DGM", "RIX", "M13", "TMP", "SCG", "BFZ", "ZEN", "VIS", "M19", "LGN", "SOK", "M10", "BNG", "8ED", "RNA",
+  "EVE", "SOM", "CMD", "DDF", "CON", "INV", "DOM", "XLN", "MIR", "AVR", "JUD", "SHM", "TD2", "TOR", "PZ1", "GTC", "SOI", "BBD", "PLS", "RTR",
+  "APC", "ORI", "IMA", "PC2", "LRW", "7ED", "M15", "M11", "KLD", "C14", "C19", "MBS", "ELD", "TD0", "ONS", "GRN", "AKH", "OGW", "WAR", "THB",
+  "M12", "C13", "MH1", "10E", "NPH", "STH", "GPT", "WTH", "PLC", "M20", "KTK", "A25", "ALA", "ODY", "ULG", "EXP", "HOU", "NEM", "ISD", "TSP",
+  "ARB", "MM2", "DTK", "TPR", "CHK", "M14", "ME3", "TSB", "VMA", "BOK", "LEA", "DKA", "PRM", "JOU", "MMQ", "EMN", "FRF", "CSP"
+];
+
 async function fetch() {
   logger.info("Checking boosterRules repository");
   const repo = await axios.get(REPO_URL);
@@ -17,7 +26,9 @@ async function fetch() {
   const resp = await axios.get(URL);
   const rules = resp.data.reduce((acc, { code, boosters, sheets }) => {
     const totalWeight = boosters.reduce((acc, { weight }) => acc + weight, 0);
-
+    if (!whitelistSets.includes(code.toUpperCase())) {
+      return acc;
+    }
     acc[code.toUpperCase()] = {
       totalWeight,
       boosters,
